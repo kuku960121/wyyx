1837020843c0c4c324d026206cbd55575ace7b79
kuku960121 <810505440@qq.com>
2020-11-26 12:50:46 +0800

    甄选家完成

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 var mySwiper = new Swiper('.swiper-container',{
     slidesPerView : 3,
     slidesPerGroup : 1,
-    autoplay:false,
-    loop: false,
+    autoplay:true,
+    loop: true,
+
+
+ /*    navigation: {//左右按钮
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }, */
 })
 
 //鼠标覆盖停止自动切换
-/* mySwiper.el.onmouseover = function(){
+ mySwiper.el.onmouseover = function(){
     mySwiper.autoplay.stop();
   }
   
 //鼠标离开开始自动切换
 mySwiper.el.onmouseout = function(){
     mySwiper.autoplay.start();
-  } */
+  } 
